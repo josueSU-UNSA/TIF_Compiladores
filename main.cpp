6cbbfd04a0142bdcccfbfb499b986a468ddd06c9
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-08-13 13:36:40 -0500

    Added a comentary which contains the our grammar

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include<fstream>
 #include"Syntax.h"
 using namespace std;
-// ofstream t;
-// ifstream aux_to_t;
+//-----------------------------------------------------------------
+// Our Spanish Programming language Syntax 
+//<Prog>::<Inicio><Bloque>"Final"
+//<Inicio>::<Forma>'\n'
+//<Forma>::"Tortuga"|"Cuadrado"|"Flecha"|"Circulo"|"Triangulo"|"Clasico"
+
+//<Bloque>:: avanzar(<DIST>)'n'<Bloque>
+//<Bloque>::gira_izquierda(<DIST>)'n'<Bloque>
+//<Bloque>::gira_derecha(<DIST>)'n'<Bloque>
+//<Bloque>::'\n'
+
+//<DIST>::number
+// --------------------------------------------------------------------
+
 int main(){
     Syntax test("testing_class_Syntax.txt","code_python_generated");
     (test.run_code())?cout<<"Succesfully execution\n":cout<<"There was a Mistake in the execution\n";
