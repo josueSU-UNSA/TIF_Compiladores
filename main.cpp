084ff5b7d52b724af1895b4f1a8087c43f27f7c7
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-08-22 22:55:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,32 +19,7 @@ using namespace std;
 int main(){
     Syntax test("testing_class_Syntax.txt","code_python_generated");
     (test.run_code())?cout<<"Succesfully execution\n":cout<<"There was a Mistake in the execution\n";
-        // string hola="hello";
-    // string instruction_to_run_code="py "+hola+".py";
-    // char *instruction_to_run_code_char_array=new char[instruction_to_run_code.size()+1];
-    // for(int i=0;i<instruction_to_run_code.size();i++){
-    //     instruction_to_run_code_char_array[i]=instruction_to_run_code[i];
-    // }
-    // instruction_to_run_code_char_array[instruction_to_run_code.size()]='\0';
-    // for(int i=0;i<instruction_to_run_code.size();i++){
-    //     cout<<instruction_to_run_code_char_array[i];
-    // }
 
-
-    // string code_py="";
-    // t.open("Hello.py");
-    // aux_to_t.open("test.txt");
-    // while(!aux_to_t.eof()){
-    //     getline(aux_to_t,code_py);
-    //     t<<code_py<<endl;
-    // }
-
-    // aux_to_t.close();
-    // t.close();
-    // if(analisis(test.txt)==true)system("py Hello.py");
-    // else {
-    //     // cout<""
-    // }
     
     return 1;
 }
